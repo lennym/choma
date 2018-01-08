6573904749380329a98e0dc2462a38cb4c7c7d65
Leonard Martin <leonard.martin@gmail.com>
2018-01-08 16:29:50 +0000

    Shuffle tests and suites within a file
    As well as only shuffling file order, also shuffle tests and sub-suites within a single file to ensure maximum randomness.

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const Mocha = require('mocha');
 const shuffle = require('lodash.shuffle');
 
 const run = Mocha.prototype.run;
+const each = Mocha.Suite.prototype.eachTest;
 
 Mocha.prototype.run = function () {
   this.files = shuffle(this.files);
   return run.apply(this, arguments);
+};
+
+Mocha.Suite.prototype.eachTest = function () {
+  this.tests = shuffle(this.tests);
+  this.suites = shuffle(this.suites);
+  return each.apply(this, arguments);
 };
