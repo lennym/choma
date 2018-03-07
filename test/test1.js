36770c0fec2126acbeb9121c70496a7b1d73c8cd
Rolf Timmermans <rolftimmermans@voormedia.com>
2018-03-07 10:35:18 +0100

    Add seeding capabilities.

---
+++ test/test1.js
@@ -0,0 +1,21 @@
+const assert = require("assert")
+
+describe("file 1 suite 1", function() {
+  it("test A", function() {
+    assert.ok(true)
+  })
+
+  it("test B", function() {
+    assert.ok(true)
+  })
+})
+
+describe("file 1 suite 2", function() {
+  it("test C", function() {
+    assert.ok(true)
+  })
+
+  it("test D", function() {
+    assert.ok(true)
+  })
+})
