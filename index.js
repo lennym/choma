lennym/choma

commit 36770c0fec2126acbeb9121c70496a7b1d73c8cd

Add seeding capabilities.

index.js
@@ -1,16 +1,46 @@
-const Mocha = require('mocha');
-const shuffle = require('lodash.shuffle');
+const Mocha = require("mocha")
+const path = require("path")
+const seedrandom = require("seedrandom")
 
-const run = Mocha.prototype.run;
-const each = Mocha.Suite.prototype.eachTest;
+const run = Mocha.prototype.run
+const each = Mocha.Suite.prototype.eachTest
+
+const prng = function() {
+  let seed = process.env.SEED || createSeed(10)
+  console.log("SEED=" + seed + " mocha")
+  return seedrandom(seed)
+}()
 
 Mocha.prototype.run = function () {
-  this.files = shuffle(this.files);
-  return run.apply(this, arguments);
-};
+  shuffle(this.files)
+  return run.apply(this, arguments)
+}
 
 Mocha.Suite.prototype.eachTest = function () {
-  this.tests = shuffle(this.tests);
-  this.suites = shuffle(this.suites);
-  return each.apply(this, arguments);
-};
+  shuffle(this.tests)
+  shuffle(this.suites)
+  return each.apply(this, arguments)
+}
+
+function shuffle(array) {
+  if (array == null || !array.length) return
+
+  let index = -1
+  const length = array.length
+  const lastIndex = length - 1
+  while (++index < length) {
+    const rand = index + Math.floor(prng() * (lastIndex - index + 1))
+    const value = array[rand]
+    array[rand] = array[index]
+    array[index] = value
+  }
+}
+
+function createSeed(length) {
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+  let text = ""
+  for (let i = 0; i < length; i++) {
+    text += chars.charAt(Math.floor(Math.random() * chars.length))
+  }
+  return text
+}

test/mocha.opts
@@ -1,0 +1,1 @@
+test --require . --reporter list

test/test1.js
@@ -1,0 +1,21 @@
+const assert = require("assert")
+
+describe("file 1 suite 1", function() {
+  it("test A", function() {
+    assert.ok(true)
+  })
+
+  it("test B", function() {
+    assert.ok(true)
+  })
+})
+
+describe("file 1 suite 2", function() {
+  it("test C", function() {
+    assert.ok(true)
+  })
+
+  it("test D", function() {
+    assert.ok(true)
+  })
+})

test/test2.js
@@ -1,0 +1,21 @@
+const assert = require("assert")
+
+describe("file 2 suite 1", function() {
+  it("test E", function() {
+    assert.ok(true)
+  })
+
+  it("test F", function() {
+    assert.ok(true)
+  })
+})
+
+describe("file 2 suite 2", function() {
+  it("test G", function() {
+    assert.ok(true)
+  })
+
+  it("test H", function() {
+    assert.ok(true)
+  })
+})

commit 5408a0cc6dc78c11e9ed6816a22b0a4c5dd86941

Fix Syntax Error on Node.js v4
Use of block-scoped declarations (let, const, etc.) without strict mode enabled leads to a syntax error in Node.js v4:

````
/Users/jhecking/aerospike/aerospike-client-nodejs/node_modules/choma/index.js:28
  let index = -1;
  ^^^

SyntaxError: Block-scoped declarations (let, const, function, class) not yet supported outside strict mode
````

This issue first started appearing in v1.2 and was introduced in 36770c0fec2126acbeb9121c70496a7b1d73c8cd I believe.

Note sure if support for Node.js v4 is a priority for choma. But I use choma in my own [project](https://github.com/aerospike/aerospike-client-nodejs) which unfortunately still needs to support Node.js v4 for now.

index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const Mocha = require('mocha');
 const seedrandom = require('seedrandom');
 const color = require('chalk');